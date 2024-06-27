b354204f5c09ab4eb6680875d31584573148b621
wessam aftab <wisamkayani360@gmail.com>
2024-06-27 12:09:18 +0500

    call back and promises
    we have studied callback,pyramid or dome , promises , chaining and error handling , async nature , event loop , queue and stack

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(`hello`)
+// callback : 
+function add(cb){
+    let a =2
+    cb()
+}
+function ratio(){
+let b =30;
+console.log(b)
+return 20;
+}
+
+
+// let b = setTimeout(ratio,100)
+
+// prmosies : 
+new Promise(()=>{
+console.log(`accepted`)
+},()=>{
+console.log(`rejected`)
+})
+console.log(`i am sync`)
